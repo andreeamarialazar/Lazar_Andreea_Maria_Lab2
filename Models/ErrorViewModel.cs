c783e1871e81bf940710cf5ea47486e5201bc326
ANDREEA-MARIA LAZAR <andreea.maria.lazar@stud.ubbcluj.ro>
2021-10-05 20:32:56 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lazar_Andreea_Maria_Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
