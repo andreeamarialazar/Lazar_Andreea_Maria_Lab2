1672f3a04b220ed223d602f57a0c907f2722e44f
ANDREEA-MARIA LAZAR <andreea.maria.lazar@stud.ubbcluj.ro>
2021-10-31 18:22:41 +0200

    bbb

---
+++ Models/LibraryViewModels/OrderGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lazar_Andreea_Maria_Lab2.Models.LibraryViewModels
+{
+    public class OrderGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? OrderDate { get; set; }
+        public int BookCount { get; set; }
+
+    }
+}
